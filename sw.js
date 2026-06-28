9db61fbea43b7588596c5b6cda9d417d155d22f7
k98143869-a11y <k98143869@gmail.com>
2026-06-29 05:03:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-self.addEventListener("install", () => {
-  console.log("PWA installed");
-});
+self.addEventListener("install",()=>{});
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener("fetch",event=>{
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
 });
